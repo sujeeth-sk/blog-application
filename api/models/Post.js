33565d96a5232f6357d1b39825a330e32ee90585
BotS3k <sujeeth2004@gmail.com>
2025-01-22 19:33:04 +0530

    update

---
+++ api/models/Post.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const PostSchema = new Schema({
+    title: String,
+    summary: String,
+    content: String,
+    cover: String,
+    author: {type:Schema.Types.ObjectId, ref: 'User'}
+}, {
+    timestamps: true
+})
+
+const PostModel = model('Post', PostSchema)
+
+export default PostModel
---
+++ api/secret.js
@@ -0,0 +1,3 @@
+export const PORT = 4000;
+// export const mongoDB_URI = 'mongodb+srv://blog-main-man:87Yhd5MXq9I6JVZy@cluster0-blog-app.cemqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-blog-app';
+export const mongoDB_URI = 'mongodb://localhost:27017/'
