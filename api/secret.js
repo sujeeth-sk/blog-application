08051614d9d3239ae11f737501d3d9d574207397
BotS3k <sujeeth2004@gmail.com>
2025-01-22 19:33:39 +0530

    update

--- api/secret.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 4000;
-// export const mongoDB_URI = 'mongodb+srv://blog-main-man:87Yhd5MXq9I6JVZy@cluster0-blog-app.cemqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-blog-app';
-export const mongoDB_URI = 'mongodb://localhost:27017/'
